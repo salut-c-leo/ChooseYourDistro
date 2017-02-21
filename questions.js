d2133c7016c4091cd01f871f296eb72fd6470197
Monsieur Tino <mrtino@users.noreply.github.com>
2017-02-21 10:18:46 +0100

    Displaying the "Recommencer" button correctly now

--- questions.js
+++ questions.js
@@ -1,33 +1,39 @@
 $('.btn-infosi').click(function() {
     $('#infosi').hide();
     $('#ootb').show();
+    $('.btn-restart').show();
 });
 $('.btn-ootb').click(function() {
     $('#ootb').hide();
     $('#old').show();
+    $('.btn-restart').show();
 });
-
 $('.btn-olds').click(function() {
     $('#old').hide();
     $('#rolling').show();
+    $('.btn-restart').show();
 });
 $('.btn-oldd').click(function() {
     $('#old').hide();
     $('#vold').show();
+    $('.btn-restart').show();
 });
 $('.btn-vold').click(function() {
     $('#vold').hide();
     $('#rolling').show();
+    $('.btn-restart').show();
 });
 $('.btn-rolling').click(function() {
     $('#rolling').hide();
     $('#inssi').show();
+    $('.btn-restart').show();
 });
 $('.btn-inssi').click(function() {
     $('#inssi').hide();
     $('#Result').show();
 });
 $('.btn-restart').click(function() {
+    $('.btn-restart').hide();
     $('#infosi').show();
     $('#ootb').hide();
     $('#old').hide();
